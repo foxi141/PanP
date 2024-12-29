1d2c9c70e73318d91ee832a3cbb34347497d41e2
Patryk Kosicki <kosicki864@gmail.com>
2024-12-30 00:31:18 +0100

    test

---
+++ Kalkulator paliwka/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Kalkulator_paliwka.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Kalkulator_paliwka.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
